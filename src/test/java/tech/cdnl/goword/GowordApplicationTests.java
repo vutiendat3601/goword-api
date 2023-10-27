75201becd8a96564c1df6a5e0fd1cd231f99c8bf
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-10-27 15:26:07 +0700

    test: add Spring App Context test

---
+++ src/test/java/tech/cdnl/goword/GowordApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.cdnl.goword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
